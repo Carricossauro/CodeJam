2b98e92f24ec87d451fc70094a56c5514b41a696
Tiago Carriço <tiagocarrico27@gmail.com>
2021-02-28 13:08:31 +0000

    ESAbATAd.py

--- 2020/ESabATAd.py
+++ 2020/ESabATAd.py
@@ -1,8 +1,11 @@
 # I can't believe I'm actually doing this ffs
+# Python solved my IO problems but now it only passes the first test
+# Idk what's wrong, it works when tested manually
+# Didn't coment the code because there is no more patience left
 import sys
 
 def getNextNumbers(k, B):
-    print(k+1, flush=True) # Give position to get
+    print(k+1, flush=True)
 
     inp = input()
     if inp == "N":
@@ -41,6 +44,7 @@ def fixReversal(array):
 def storeNumbers(array, num1, num2, k, B):
     array[k] = num1
     array[B-k-1] = num2
+    return array
 
 def fixArray(array, complementation, reversal):
     if complementation:
@@ -51,47 +55,47 @@ def fixArray(array, complementation, reversal):
 
 def main():
     [T, B] = list(map(int,input().split()))
-    array = [0] * B
-    for x in range(T):
+    for _ in range(T):
+        array = [0] * B
         k = 0
         total = 2
         samePairk = diffPairk = -1
         queries = 2
         comp = rev = False
 
-        nums = getNextNumbers(k, B)
+        nums = getNextNumbers(0, B)
         storeNumbers(array, nums[0], nums[1], k, B)
         k+=1
 
-        if samePairk == -1 and nums[0] == nums[1]:
+        if nums[0] == nums[1]:
             samePairk = k-1
-        elif diffPairk == -1 and nums[0] == nums[1]:
+        elif nums[0] != nums[1]:
             diffPairk = k-1
 
         while total < B:
             while queries % 10 != 0:
                 nums = getNextNumbers(k, B)
-                storeNumbers(array, nums[0], nums[1], k, B)
+                array = storeNumbers(array, nums[0], nums[1], k, B)
                 k += 1
                 queries += 2
                 
                 if samePairk == -1 and nums[0] == nums[1]:
                     samePairk = k-1
-                elif diffPairk == -1 and nums[0] == nums[1]:
+                elif diffPairk == -1 and nums[0] != nums[1]:
                     diffPairk = k-1
                 
                 total += 2
                 if total >= B:
                     break
             
+            comp = rev = False
             if total < B:
-                comp = rev = False
-                if samePairk != 0:
+                if samePairk != -1:
                     num = getNumber(samePairk)
                     queries += 1
                     if array[samePairk] != num:
                         comp = True
-                if diffPairk != 0:
+                if diffPairk != -1:
                     num = getNumber(diffPairk)
                     queries += 1
                     if array[diffPairk] != num:
@@ -99,11 +103,11 @@ def main():
                             rev = True
                     elif comp:
                         rev = True
-            fixArray(array, comp, rev)
+            array = fixArray(array, comp, rev)
 
         print("".join([str(x) for x in array]), flush=True)
         result = input()
-        if result == "N" or result != "Y":
+        if result == "N":
             return -1
     return 0
 
